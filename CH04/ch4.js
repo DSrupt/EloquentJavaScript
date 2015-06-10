0b5185434e83af03a6a8a7e05614acad6818625a
Akhil Thampy <akhilthampy@yahoo.com>
2015-06-10 15:16:49 -0500

    Playing around with ch4

---
+++ CH04/ch4.js
@@ -0,0 +1,8 @@
+var array = [1,1,2,1,3,2,124,1,2];
+
+for(var i=0;i<array.length;i++){
+	console.log(array[i]);
+}
+console.log("Array length is :\t"+array['length']);
+
+console.log("DunDunDun".toUpperCase());
